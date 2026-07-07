ab0a51f8d338be4f9bc6bd256663f651fcca3208
Yash Patil <yp.atil2040@gmail.com>
2026-07-07 19:51:20 +0530

    Updated project files

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, session, logging
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, RadioField, SelectField, IntegerField
-from wtforms.fields.html5 import DateField
+try:
+	from wtforms.fields import DateField
+except ImportError:
+	from wtforms.fields.html5 import DateField
 from passlib.hash import sha256_crypt
-from flask_script import Manager
 from functools import wraps
 from datetime import datetime
 
@@ -689,9 +691,5 @@ def logout():
 
 
 if __name__ == "__main__":
-	app.secret_key = '528491@JOKER'
-	app.debug = True
-	manager = Manager(app)
-	#manager.secret_key = '528491@siva'
-	manager.run()
-	#app.run()
+    app.secret_key = '528491@JOKER'
+    app.run(debug=True)
